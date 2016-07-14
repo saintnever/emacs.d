66e29eb770507169fe2121fb3b0d111653279e87
TX Z <TX Z>
2016-07-14 13:03:11 +0800

    1.add smex configs 2.add smex-itme

--- init.el
+++ init.el
@@ -22,7 +22,8 @@
     flycheck
     material-theme
     py-autopep8
-    magit))
+    magit
+    smex))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -63,4 +64,53 @@
 ;; chinese XiaoHe input
 (require 'flypy)
 (custom-set-variables '(default-input-method "chinese-flypy"))
+
+;;smex
+(autoload 'smex "smex")
+(global-set-key (kbd "M-x") 'smex)
+;;hyphen on space
+    (defadvice smex (around space-inserts-hyphen activate compile)
+        (let ((ido-cannot-complete-command 
+               `(lambda ()
+                  (interactive)
+                  (if (string= " " (this-command-keys))
+                      (insert ?-)
+                    (funcall ,ido-cannot-complete-command)))))
+          ad-do-it))
+;;update on load file
+   (defun smex-update-after-load (unused)
+      (when (boundp 'smex-cache)
+        (smex-update)))
+(add-hook 'after-load-functions 'smex-update-after-load)
+;;use acronyms
+;;; Filters ido-matches setting acronynm matches in front of the results
+(defadvice ido-set-matches-1 (after ido-smex-acronym-matches activate)
+  (if (and (fboundp 'smex-already-running) (smex-already-running)
+           (> (length ido-text) 1))
+      (let ((regex (concat "^" (mapconcat 'char-to-string ido-text "[^-]*-")))
+            (acronym-matches (list))
+            (remove-regexes '("-menu-")))
+        ;; Creating the list of the results to be set as first
+        (dolist (item items)
+          (if (string-match ido-text item) ;; exact match
+              (add-to-list 'acronym-matches item)
+            (if (string-match (concat regex "[^-]*$") item) ;; strict match
+                (add-to-list 'acronym-matches item)
+              (if (string-match regex item) ;; appending relaxed match
+                  (add-to-list 'acronym-matches item t)))))
+
+        ;; Filtering ad-return-value
+        (dolist (to_remove remove-regexes)
+          (setq ad-return-value
+                (delete-if (lambda (item)
+                             (string-match to_remove item))
+                           ad-return-value)))
+
+        ;; Creating resulting list
+        (setq ad-return-value
+              (append acronym-matches
+                      ad-return-value))
+
+        (delete-dups ad-return-value)
+        (reverse ad-return-value))))
 ;; init.el ends here
